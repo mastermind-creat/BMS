2988631458df5f9fe19a509b5e32aba48a7f694b
Kennedy Wambia <kennyleyy0@gmail.com>
2025-06-11 17:10:42 +0300

    changed database name from bursary to gla

--- config/database.php
+++ config/database.php
@@ -2,21 +2,20 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'bursary');
+define('DB_NAME', 'gla');
 
 // Attempt to connect to MySQL database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
 // Check connection
-if($conn === false){
+if ($conn === false) {
     die("ERROR: Could not connect to database. " . mysqli_connect_error());
 }
 
 // Create database if it doesn't exist
 $sql = "CREATE DATABASE IF NOT EXISTS " . DB_NAME;
-if(mysqli_query($conn, $sql)){
+if (mysqli_query($conn, $sql)) {
     mysqli_select_db($conn, DB_NAME);
 } else {
     die("ERROR: Could not create database. " . mysqli_error($conn));
 }
-?> 
